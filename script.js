1c4327e7dbdda19ce9d967654c7ae8712bb89a27
LENOVO-PC\Urszula <ugrudzien12@gmail.com>
2018-06-28 11:05:48 +0200

    disappearing circles

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 ///dodanie lementów do nowej tablicy
 
-var table=[5,6,8,3,4];
-var table2=[1,2];
+// var table=[5,6,8,3,4];
+// var table2=[1,2];
 
-table.forEach(el=> table2.push(el*el));
-//table2 = table.push()
+// var obj = {
+//     "desc": "jakis opis",
+//     "count": 4,
+//     "data": {
+//         "abs": true
+//     }
+// }
+
+// obj.fn();
+
+// table.forEach(el=> table2.push(el*el));
+// //table2 = table.push()
 
 
-console.log(table2);
+// console.log(table2);
+
+// console.log(obj.desc);
+
+document.getElementById("redCircle").onclick = function(){
+    document.getElementById("redCircle").style.display = "none";
+}
+document.getElementById("blueCircle").onclick = function(){
+    document.getElementById("blueCircle").style.display = "none";
+}
+document.getElementById("greenCircle").onclick = function(){
+    document.getElementById("greenCircle").style.display = "none";
+}
